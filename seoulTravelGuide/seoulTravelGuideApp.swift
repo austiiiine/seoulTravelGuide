230d1aab64c132c845eb6e317d7711531e7671b0
Austine Huang <austine.huang@gmail.com>
2025-11-01 15:38:22 +0800

    Initial Commit

---
+++ seoulTravelGuide/seoulTravelGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  seoulTravelGuideApp.swift
+//  seoulTravelGuide
+//
+//  Created by Austine Huang on 2025/11/1.
+//
+
+import SwiftUI
+
+@main
+struct seoulTravelGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
